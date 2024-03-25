a330d84fca3891ed6662ad25718e2696cfa79db1
ravikanth1980 <b.ravikanthnb@gmail.com>
2024-03-25 17:09:19 +0530

    Variables added

--- variable.tf
+++ variable.tf
@@ -4,3 +4,10 @@ variable "ec2_ami" {
     default = "ami-05295b6e6c790593e" 
   
 }
+
+variable "ec2_inst_type" {
+    description = "Instance_type"
+    type = "string"
+    default = "t2.micro" 
+  
+}
